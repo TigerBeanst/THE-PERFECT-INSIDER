f1485caf34dffcd9ee344886c35d9c48e1e7cef6
hjthjthjt <baidu051033@163.com>
2016-04-17 12:37:49 +0800

    Add uninstall

--- gen/com/jakting/fff/R.java
+++ gen/com/jakting/fff/R.java
@@ -10,6 +10,9 @@ package com.jakting.fff;
 public final class R {
     public static final class attr {
     }
+    public static final class id {
+        public static final int uninstall=0x7f060000;
+    }
     public static final class layout {
         public static final int main=0x7f030000;
     }
@@ -19,6 +22,7 @@ public final class R {
     public static final class string {
         public static final int fff=0x7f040000;
         public static final int fff_des=0x7f040001;
+        public static final int uninstall=0x7f040002;
     }
     public static final class style {
         public static final int AppTheme=0x7f050000;
